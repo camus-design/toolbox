adda7a1350ed6fd77a3a3fd58d5a858a910308d9
nepaul <xs.nepaul@gmail.com>
2023-03-16 11:48:55 +0800

    feat(launcher): phone call

---
+++ lib/src/flutter/launcher.dart
@@ -0,0 +1,12 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class CaumsLauncer {
+  static Future<void> launchPhoneCall(String phoneNumber) async {
+    final Uri uri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $phoneNumber';
+    }
+  }
+}
