9f70b9b2531fd078c85779f4f1037d1f590eca25
nepaul <xs.nepaul@gmail.com>
2023-03-22 11:01:33 +0800

    feat(date): interval timer

---
+++ lib/src/date/interval_timer.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+class CamusIntervalTimer {
+  CamusIntervalTimer(this.interval, this.callback);
+
+  final Duration interval;
+  final void Function() callback;
+  late Timer _timer;
+
+  void start() {
+    _timer = Timer.periodic(interval, (timer) {
+      callback();
+    });
+  }
+
+  void stop() {
+    _timer?.cancel();
+  }
+}
