c6ac3c6f240696f33d75ffca25402f00d521f515
nepaul <xs.nepaul@gmail.com>
2023-04-01 21:51:42 +0800

    doc: string

--- lib/src/extentions/string.dart
+++ lib/src/extentions/string.dart
@@ -1,12 +1,36 @@
+/// Extension methods for the `String` class.
 extension StringExtension on String {
+  /// Returns the reversed version of the string.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// 'hello'.reverse(); // returns 'olleh'
+  /// ```
   String reverse() {
     return String.fromCharCodes(runes.toList().reversed);
   }
 
+  /// Returns `true` if the string represents a number.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// '123'.isNumber(); // returns true
+  /// 'abc'.isNumber(); // returns false
+  /// ```
   bool isNumber() {
     return double.tryParse(this) != null;
   }
 
+  /// Returns `true` if the string represents a digit.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// '1'.isDigit(); // returns true
+  /// 'a'.isDigit(); // returns false
+  /// ```
   bool isDigit() {
     if (isEmpty) {
       return false;
@@ -22,6 +46,20 @@ extension StringExtension on String {
     return runes.first ^ 0x30 <= 9;
   }
 
+  /// Adds the given character at the specified position.
+  ///
+  /// If `repeat` is `false`, the character is added only once at the specified
+  /// position. Otherwise, the character is repeatedly added at every `position`
+  /// index of the string.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// 'hello'.addCharAtPosition('*', 2); // returns 'he*llo'
+  /// 'hello'.addCharAtPosition('*', 2, repeat: true); // returns 'he*l*l*o'
+  /// ```
+  ///
+  /// Throws an `AssertionError` if `position` is out of range.
   String addCharAtPosition(
     String char,
     int position, {
@@ -45,6 +83,13 @@ extension StringExtension on String {
     return buffer.toString();
   }
 
+  /// Returns the string with zero-width space characters between each character.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// 'hello'.toCharacterBreakString(); // returns 'h​e​l​l​o'
+  /// ```
   String toCharacterBreakString() {
     if (isEmpty) {
       return this;
@@ -52,10 +97,23 @@ extension StringExtension on String {
     return runes.map(String.fromCharCode).join('\u{200B}');
   }
 
+  /// Returns the string with the first character capitalized.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// 'hello'.capitalize(); // returns 'Hello'
+  /// ```
   String capitalize() {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
 
+  /// Splits the string into chunks of the specified size.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// 'hello world'.toChunks(3); // returns ['hel', 'lo ', 'wor', 'ld']
   List<String> toChunks(int size) {
     final List<String> chunks = <String>[];
     for (int i = 0; i < length; i += size) {
--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -2,7 +2,9 @@ library toolbox;
 
 export 'src/empty.dart';
 export 'src/extentions/datetime.dart';
+export 'src/extentions/list.dart';
 export 'src/extentions/number.dart';
 export 'src/flutter/device.dart';
 export 'src/flutter/launcher.dart';
 export 'src/log/logger.dart';
+export 'src/extentions/string.dart';
