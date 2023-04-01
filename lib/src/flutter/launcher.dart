7fa89d87ecaeb6aefb198d92a5375a0b5c4593b9
nepaul <xs.nepaul@gmail.com>
2023-04-01 21:34:28 +0800

    doc: launcher

--- lib/src/flutter/launcher.dart
+++ lib/src/flutter/launcher.dart
@@ -1,12 +1,38 @@
 import 'package:url_launcher/url_launcher.dart';
 
+/// A utility class for launching phone calls and URLs.
 class CamusLauncher {
+  /// Launches a phone call with the given phone number.
+  ///
+  /// Throws an exception if the phone call could not be launched.
+  ///
+  /// Example:
+  /// ```dart
+  /// await CamusLauncher.launchPhoneCall('1234567890');
+  /// ```
   static Future<void> launchPhoneCall(String phoneNumber) async {
     final Uri uri = Uri(scheme: 'tel', path: phoneNumber);
     if (await canLaunchUrl(uri)) {
       await launchUrl(uri);
     } else {
       throw 'Could not launch $phoneNumber';
+    }
+  }
+
+  /// Opens a URL with the given [url] string.
+  ///
+  /// Throws an exception if the URL could not be launched.
+  ///
+  /// Example:
+  /// ```dart
+  /// await CamusLauncher.openUrl('https://www.example.com');
+  /// ```
+  static Future<void> openUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
     }
   }
 }
--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -2,4 +2,5 @@ library toolbox;
 
 export 'src/empty.dart';
 export 'src/flutter/device.dart';
+export 'src/flutter/launcher.dart';
 export 'src/log/logger.dart';
