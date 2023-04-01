caf09a64ad8c3c8c958817a48989ccd01c7b6464
nepaul <xs.nepaul@gmail.com>
2023-04-01 21:42:56 +0800

    doc: number

--- lib/src/extentions/number.dart
+++ lib/src/extentions/number.dart
@@ -1,11 +1,25 @@
-/// Extends the [num] with additional functionality.
+/// Extension that provides additional methods to work with numbers.
 extension NumberExtension on num {
   /// Returns a string representation of this number with the given [length].
+  ///
+  /// If this number is shorter than [length], it will be padded with the [pad] character
+  /// (defaults to '0') to match the specified length.
+  ///
+  /// Example:
+  /// ```dart
+  /// print(2.padLeftNumber(3)); // '002'
+  /// ```
   String padLeftNumber(num number, int length, {String pad = '0'}) {
     return number.toString().padLeft(length, pad);
   }
 
-  /// Returns true if this number is between [min] and [max] (inclusive).
+  /// Returns `true` if this number is between [min] and [max] (inclusive).
+  ///
+  /// Example:
+  /// ```dart
+  /// print(5.isBetween(1, 10)); // true
+  /// print(5.isBetween(10, 20)); // false
+  /// ```
   bool isBetween(num min, num max) {
     return this >= min && this <= max;
   }
--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -2,6 +2,7 @@ library toolbox;
 
 export 'src/empty.dart';
 export 'src/extentions/datetime.dart';
+export 'src/extentions/number.dart';
 export 'src/flutter/device.dart';
 export 'src/flutter/launcher.dart';
 export 'src/log/logger.dart';
