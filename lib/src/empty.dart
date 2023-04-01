d698fe96ca918c1d01364a4d09fae77514937965
nepaul <xs.nepaul@gmail.com>
2023-04-01 21:10:40 +0800

    doc: empty.dart

--- lib/src/empty.dart
+++ lib/src/empty.dart
@@ -1,3 +1,13 @@
+/// Returns true if [value] is not null and is not empty.
+///
+/// If [value] is a String, this function checks whether it has a non-zero length.
+/// If [value] is a List, Set, or Map, this function checks whether it has a non-zero length.
+/// If [value] is any other type, this function simply checks whether it is not null.
 bool isNotNullOrEmpty(dynamic value) => value != null && value!.isNotEmpty;
 
+/// Returns true if [value] is null or empty.
+///
+/// If [value] is a String, this function checks whether it has a zero length.
+/// If [value] is a List, Set, or Map, this function checks whether it has a zero length.
+/// If [value] is any other type, this function simply checks whether it is null.
 bool isNullOrEmpty(dynamic value) => !isNotNullOrEmpty(value);
--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -1,3 +1,4 @@
 library toolbox;
 
+export 'src/empty.dart';
 export 'src/log/logger.dart';
