6d1db95f49f9ac0bf0e7f189c8235f9bf1d8db58
nepaul <xs.nepaul@gmail.com>
2023-04-01 21:55:42 +0800

    doc: intervaltimer

--- lib/src/date/interval_timer.dart
+++ lib/src/date/interval_timer.dart
@@ -1,18 +1,47 @@
 import 'dart:async';
 
+/// A class for creating a timer that triggers a callback function at a fixed interval.
 class CamusIntervalTimer {
+  /// Creates a timer that triggers a callback function at a fixed interval.
   CamusIntervalTimer(this.interval, this.callback);
 
+  /// The fixed time interval at which [callback] is called.
   final Duration interval;
+
+  /// The function that is called at fixed time intervals.
   final void Function() callback;
   late Timer _timer;
 
+  /// Starts the timer.
+  ///
+  /// Example:
+  /// ```
+  /// final timer = CamusIntervalTimer(Duration(seconds: 1), () {
+  ///   print('Hello, world!');
+  /// });
+  ///
+  /// timer.start();
+  /// ```
   void start() {
     _timer = Timer.periodic(interval, (Timer timer) {
       callback();
     });
   }
 
+  /// Stops the timer.
+  ///
+  /// Example:
+  /// ```
+  /// final timer = CamusIntervalTimer(Duration(seconds: 1), () {
+  ///   print('Hello, world!');
+  /// });
+  ///
+  /// timer.start();
+  ///
+  /// // Wait for 5 seconds
+  ///
+  /// timer.stop();
+  /// ```
   void stop() {
     _timer.cancel();
   }
